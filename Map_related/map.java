2e085d75b21e6b91e354046b75a0c75b0afafa17
Md Al Amin Tokder Shoukhin <62458402+Shoukhin1803078@users.noreply.github.com>
2022-04-06 07:49:46 +0600

    Update map.java

--- Map_related/map.java
+++ Map_related/map.java
@@ -1,1 +1,22 @@
 
+
+import java.util.*;
+
+public class NewClass_1644_A{
+
+	public static void main(String arg[]){
+		Scanner ob=new Scanner(System.in);
+	
+			String s=ob.next();
+			Map<Character,Integer> mp=new HashMap();
+			mp.put('r',0);
+			mp.put('b',0);
+			mp.put('g',0);
+			
+			for(int i=0;i<s.length();i++){
+				mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
+      }
+
+  }
+		
+}
